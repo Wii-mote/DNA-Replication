52a4f454e13bf56fdca44d414f82b4fb6eab81fb
Will Blanchard <blanchardpwilliam3@gmail.com>
2024-09-01 23:03:47 +0200

    Version 1.0.0.5
    Final Commit
    
    All methods have been tested and work as intended.
    
    Project Closed.

--- src/main/DNAReplication.java
+++ src/main/DNAReplication.java
@@ -1,4 +1,70 @@
 package main;
 
-public class DNAReplcation {
-}
+/*
+DP SL Computer Science
+@author Will Blanchard
+ */
+public class DNAReplication {
+
+    //Method to copy the dna sequence
+    public char[] copyDNAStrand(char[] strand) {
+
+        char[] copiedStrand = new char[strand.length];
+
+        for (int index = 0; index < strand.length; index++) {
+
+            if (strand[index] != 'A' && strand[index] != 'C' && strand[index] != 'G' && strand[index] != 'T') {
+
+                System.out.println("An invalid character has been found at index " + index + " please ensure your DNA strand has been entered correctly");
+
+            } else {
+
+                copiedStrand[index] = strand[index];
+
+            }//End of If-Else Statement
+
+        }//End of For Loop
+
+        return copiedStrand;
+
+    }//End of Method
+    
+    //Method to invert the dna strand so it can be replicated
+    public char[] invertDNAStrand(char[] strand) {
+        
+        //Same looping technique as before
+        int index = 0;
+        char[] invertedArray = new char[strand.length];
+        
+        for (char element : strand) {
+            
+            if (element == 'A') {
+                
+                invertedArray[index++] = 'C';
+                
+            } else if (element == 'C') {
+                
+                invertedArray[index++] = 'A';
+                
+            } else if (element == 'G') {
+
+                invertedArray[index++] = 'T';
+
+            } else if (element == 'T') {
+
+                invertedArray[index++] = 'G';
+
+            } else {
+
+                System.out.println("An invalid character has been found at index " + index + " please ensure your DNA strand has been entered correctly");
+                index++;
+
+            }//End of If-Else-If-Else Statement
+
+        }//End of Method
+
+        return invertedArray;
+        
+    }//End of Method
+
+}//End of Class
--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,4 +1,59 @@
 package main;
 
-public class Main {
-}
+import java.util.Arrays;
+
+/*
+DP SL Computer Science
+@author Will Blanchard
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+
+        DNAReplication replication = new DNAReplication();
+
+        //Instantiating an array of type char, with a dna strand in it
+        char[] dnaStrand1 = {
+
+                'A',
+                'C',
+                'C',
+                'G',
+                'A',
+                'T',
+                'T'
+
+        };
+
+        //Instantiating another array of type char with an erroneous letter in it
+        char[] dnaStrand2 = {
+
+                'C',
+                'T',
+                'G',
+                'G',
+                'F',
+                'T',
+                'A'
+
+
+        };
+
+        //Setting up test cases
+        System.out.println("Let's test the program with two DNA strands, one valid strand and one invalid (meaning it contains a character other than A, C, G or T)");
+        System.out.println("\nHere is the first strand:");
+        System.out.println(Arrays.toString(replication.copyDNAStrand(dnaStrand1)));
+        System.out.println();
+        System.out.println("\nHere is the second strand:");
+        System.out.println(Arrays.toString(replication.copyDNAStrand(dnaStrand2)));
+        System.out.println("\nThis is what the second strand's array actually looks like");
+        System.out.println(Arrays.toString(dnaStrand2));
+        System.out.println("\nNow that we know which strand is erroneous let's prepare the first strand for replication by inverting it. This is what the inverted array looks like");
+        System.out.println(Arrays.toString(replication.invertDNAStrand(replication.copyDNAStrand(dnaStrand1))));
+
+
+
+
+    }//End of Main Method
+
+}//End of Class
